60ccf9f6a9188bd0076f2f910c8894fd1d117a98
Anoop12345678 <anupsingbhandari7@gmail.com>
2024-10-15 15:15:58 +0000

    variables

---
+++ basic/1_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1345
+let accountEmail = "anup@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+
+/*
+Perfer not to use var because
+of issue in block scope and function scope
+*/
+
+
+console.log(accountId)
+console.log(accountcity)
